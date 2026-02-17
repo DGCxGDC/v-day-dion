a038b72b6ba70aa9f6401983313e241bb45cae13
DGCxGDC <dionca92@gmail.com>
2026-02-17 13:33:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ function handleNoClick() {
     swapGif(gifStages[gifIndex])
 
     // Runaway starts at click 5
-    if (noClickCount >= 9 && !runawayEnabled) {
+    if (noClickCount >= 8 && !runawayEnabled) {
         enableRunaway()
         runawayEnabled = true
     }
